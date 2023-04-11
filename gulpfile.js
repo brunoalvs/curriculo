3353ba793bf64966003258074653682d0eb871f7
Bruno Alves <hello@brunoalves.app>
2023-04-11 11:56:12 +0100

    dependencies fixed and updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,72 @@
-'use strict'
+const gulp = require('gulp');
+const stylus = require('gulp-stylus');
+const pug = require('gulp-pug');
+const browserSync = require('browser-sync');
 
-var gulp = require('gulp'),
-    stylus = require('gulp-stylus'),
-    pug = require('gulp-pug'),
-    browserSync = require('browser-sync');
-
-gulp.task('serve', ['pug', 'stylus'], function() {
-  browserSync.init({
-    server: './'
-  });
-
-  gulp.watch('assets/stylus/**/*.styl', ['stylus']);
-  gulp.watch('./**/*.pug', ['pug']);
-  gulp.watch('./**/*.{html,css}').on('change', browserSync.reload);
-});
-
-gulp.task('pug', function() {
+function pugTask() {
   return gulp.src('*.pug')
     .pipe(pug())
     .pipe(gulp.dest('./'))
     .pipe(browserSync.stream());
-})
+}
 
-gulp.task('stylus', function() {
+function stylusTask() {
   return gulp.src('assets/stylus/**/*.styl')
     .pipe(stylus())
     .on('error', function(err){
-            browserSync.notify(err.message, 3000);
-            this.emit('end');
-        })
+      browserSync.notify(err.message, 3000);
+      this.emit('end');
+    })
     .pipe(gulp.dest('assets/css'))
     .pipe(browserSync.stream());
-})
+}
 
-gulp.task('default', ['pug', 'stylus', 'serve']);
+
+function serveTask() {
+  browserSync.init({
+    server: './'
+  });
+
+  gulp.watch('assets/stylus/**/*.styl', stylusTask);
+  gulp.watch('./**/*.pug', pugTask);
+  gulp.watch('./**/*.{html,css}').on('change', browserSync.reload);
+}
+
+exports.default = gulp.series(pugTask, stylusTask, serveTask);
+
+// 'use strict'
+
+// var gulp = require('gulp'),
+//     stylus = require('gulp-stylus'),
+//     pug = require('gulp-pug'),
+//     browserSync = require('browser-sync');
+
+// gulp.task('serve', ['pug', 'stylus'], function() {
+//   browserSync.init({
+//     server: './'
+//   });
+
+//   gulp.watch('assets/stylus/**/*.styl', ['stylus']);
+//   gulp.watch('./**/*.pug', ['pug']);
+//   gulp.watch('./**/*.{html,css}').on('change', browserSync.reload);
+// });
+
+// gulp.task('pug', function() {
+//   return gulp.src('*.pug')
+//     .pipe(pug())
+//     .pipe(gulp.dest('./'))
+//     .pipe(browserSync.stream());
+// })
+
+// gulp.task('stylus', function() {
+//   return gulp.src('assets/stylus/**/*.styl')
+//     .pipe(stylus())
+//     .on('error', function(err){
+//             browserSync.notify(err.message, 3000);
+//             this.emit('end');
+//         })
+//     .pipe(gulp.dest('assets/css'))
+//     .pipe(browserSync.stream());
+// })
+
+// gulp.task('default', ['pug', 'stylus', 'serve']);
